01ed6aeb8ac77d0f8306e054ebb9a6c0db093f9d
Saman Rashidi <samanrashidi44@gmail.com>
2020-03-18 13:06:23 +0330

    fix: chagned some content and icons

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -9,23 +9,8 @@
           <div class="section-content">
             <p class="justified" v-html="AboutMe.aboutDescription"></p>
           </div>
-          <div class="pagination">
-            <div class="pagination-content">
-              <div class="pagination-item" v-for="(certificate, index) in Certificates" :key="index">
-                <a :href="require(`@/assets/images/certificates/${certificate.image}`)" :data-lightbox="certificate.name" :data-title="certificate.title"><img :src="require(`@/assets/images/certificates/${certificate.thumb}`)" :alt="getAlt(index)" /></a>
-              </div>
-            </div>
-            <div class="pagination-navigation">
-              <div class="pagination-current"></div>
-              <div class="pagination-dots">
-                <button class="pagination-dot" v-for="(certificate, index) in Certificates" :key="index">
-                  <span class="pagination-number">{{index+1}}</span>
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
-        <img itemprop="image" :class="Owner.firstname.toLowerCase()" :src="getImgUrl(AboutMe.image)" :alt="Owner.fullname" />
+        <img itemprop="image" :class="Owner.firstname.toLowerCase()" src="@/assets/images/samanrashidii.png" :alt="Owner.fullname" />
       </div>
     </div>
 </template>
@@ -37,17 +22,8 @@ export default {
   computed: {
     ...mapGetters([
         'Owner',
-        'AboutMe',
-        'Certificates'
+        'AboutMe'
     ])
-  },
-  methods: {
-    getImgUrl(pic) {
-      return require(`@/assets/images/${pic}`);
-    },
-    getAlt(index){
-      return this.Owner.fullname + ' ' + this.Certificates[index].title;
-    }
-  },
+  }
 };
 </script>
