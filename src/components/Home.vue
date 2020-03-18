61e374f02b24bfa4381567e116e5730f67b68cea
Saman Rashidi <samanrashidi44@gmail.com>
2020-03-18 16:04:42 +0330

    fix: improved UI / UX

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
                 <h1 v-html="Home.pageTitle"></h1>
                 <p>{{ Home.pageSubTitle }}</p>
                 <div class="button-holder">
-                    <a v-for="(button, index) in Home.buttons" :key="index" class="bttn has-icon" :class="button.icon" href="javascript:void(0)" :onClick="`JavaScript:$('#${button.navTarget}').animatescroll({scrollSpeed:1200,easing:'easeInExpo'})`">{{button.text}}</a>
+                    <a v-for="(button, index) in Home.buttons" :key="index" class="bttn has-icon" :class="button.icon" href="javascript:void(0)" :onClick="`JavaScript:$('#${button.navTarget}').animatescroll({scrollSpeed:1200,easing:'easeInExpo'})`"><span>{{button.text}}</span></a>
                 </div>
                 <a class="Arrow" href="javascript:void(0)" :onClick="`JavaScript:$('#${Home.navTarget}').animatescroll({scrollSpeed:1200,easing:'easeInExpo'})`"></a>
             </div>
--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -1,16 +1,18 @@
 <template>
     <div id="about-me" class="has-shape shape-4 white-color center-aligned">
       <div class="frame">
+        <div class="section-title">
+          <span>{{AboutMe.sectionSub}}</span>
+          <h1>{{AboutMe.sectionTitle}}</h1>
+        </div>
         <div class="reveal">
-          <div class="section-title">
-            <span>{{AboutMe.sectionSub}}</span>
-            <h1>{{AboutMe.sectionTitle}}</h1>
-          </div>
-          <div class="section-content">
-            <p class="justified" v-html="AboutMe.aboutDescription"></p>
+          <div class="about-wrapper">
+            <img itemprop="image" :class="Owner.firstname.toLowerCase()" src="@/assets/images/samanrashidii.png" :alt="Owner.fullname" />
+            <div class="section-content">
+              <p class="justified" v-html="AboutMe.aboutDescription"></p>
+            </div>
           </div>
         </div>
-        <img itemprop="image" :class="Owner.firstname.toLowerCase()" src="@/assets/images/samanrashidii.png" :alt="Owner.fullname" />
       </div>
     </div>
 </template>
