7ad22046d3e8ab4cbeafc37ef7c7dd11d1f00666
Saman Rashidi <samanrashidi44@gmail.com>
2020-06-01 04:11:12 +0430

    fix: minor issue

--- src/components/contact/contact_me/ContactInformation.vue
+++ src/components/contact/contact_me/ContactInformation.vue
@@ -2,8 +2,9 @@
     <div class="reveal">
         <div class="contact-info">
             <ul>
-                <li><strong class="mobile-icon">{{Contact.user.mobileText}}:</strong> <a itemprop="telephone" :href="'tel:'+Contact.user.mobile"> {{Contact.user.mobile}}</a></li>
-                <li><strong class="email-icon">{{Contact.user.emailText}}:</strong> <a :href="'mailto:'+Contact.user.email"><span itemprop="email"> {{Contact.user.email}}</span></a></li>
+                <li>
+                    <strong class="email-icon">{{Contact.user.emailText}}:</strong> <a :href="'mailto:'+Contact.user.email"><span itemprop="email"> {{Contact.user.email}}</span></a>
+                </li>
                 <li class="socials">
                     <a v-for="(social, index) in Contact.socials" :key="index" class="social-icon" :class="social.name" :href="social.url"  target="_blank"></a>
                 </li>
